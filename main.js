760756bec1ce0d972e8a3ba58869dcd55be7eb7c
Abyss <xatkx1@gmail.com>
2022-08-22 00:49:22 -0400

    termine implemetar la lsit product

--- main.js
+++ main.js
@@ -10,6 +10,11 @@ const btnMenuMobile = document.querySelector('#menuMobile');
 const btnShoppingCart = document.querySelector('.navbar-shopping-cart');
 const product_detail = document.querySelector('.product-detail')
 
+
+// card - container
+
+const containerCard = document.querySelector('.cards-container')
+
 //funciones
 function emailMenuF(event) {
     console.log()
@@ -37,3 +42,120 @@ function productDetailF(event){
 btnShoppingCart.addEventListener('click',productDetailF)
 emailMenu.addEventListener('click',emailMenuF);
 btnMenuMobile.addEventListener('click', mobileMenuF);
+
+
+let productList = [
+    {
+        name: 'bike',
+        img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 9200
+    },
+    {
+        name: 'bike',
+        img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 1200
+    },
+    {
+        name: 'iphone',
+        img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 90
+    },
+    {
+        name: 'tv',
+        img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 200
+    },
+    {
+        name: 'sayayin',
+        img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 298700
+    },{
+        name: 'tv',
+        img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 200
+    },
+    {
+        name: 'tv',
+        img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 200
+    },
+    {
+        name: 'tv',
+        img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 200
+    },
+    {
+        name: 'tv',
+        img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 200
+    },
+    {
+        name: 'tv',
+        img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 200
+    },
+    {
+        name: 'tv',
+        img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 200
+    },
+    {
+        name: 'tv',
+        img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        price: 200
+    },
+]
+
+function productos(json){
+
+    let imgA = './icons/bt_add_to_cart.svg'
+
+    let producto = document.createElement('div');
+    producto.classList.add('product-card');
+
+    let img = document.createElement('img');
+    img.setAttribute('src',json.img)
+
+    let productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    
+    
+    let price = document.createElement('p');
+    price.innerText = json.price
+    
+    let name = document.createElement('p');
+    name.innerText = json.name
+
+    let div = document.createElement('div');
+    div.appendChild(price);
+    div.appendChild(name);
+
+    let img2 = document.createElement('img');
+    img2.setAttribute('src',imgA);
+    img2.setAttribute('alt','kk');
+
+    let figure = document.createElement('figure');
+    figure.appendChild(img2);
+
+    productInfo.appendChild(div)
+    productInfo.appendChild(figure)
+
+    producto.appendChild(img);
+    producto.appendChild(productInfo);
+
+
+    return producto
+
+}
+
+productList.forEach( pro => {
+    containerCard.appendChild(productos(pro))
+} )
+
+
+// for (const product of productList) {
+// containerCard.appendChild(productos(product))
+// }
+
+
